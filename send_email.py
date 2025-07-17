b0bdbfec956ba07c3f81d65cfc0134644b0dcc0a
gvz111 <robocop220692@yahoo.com>
2025-07-17 11:37:07 +0400

    Send email using the Contact-page form

---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "robocop220692@gmail.com"
+    password = "sgacorkyasohubpl"
+
+    receiver = "robocop220692@gmail"
+    my_context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username,receiver, message)
+    
+    
